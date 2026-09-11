b46f1752386573fbb4dbb165976fb73d65690b11
Adnan Khan <adnanuitit@gmail.com>
2026-09-11 23:19:35 +0530

    Code Chnages

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-function addDetails() {
+function saveAndNext() {
 
     let name = document.getElementById("name").value;
     let age = document.getElementById("age").value;
     let accNo = document.getElementById("accNo").value;
 
+
+    // Save data in localStorage
     localStorage.setItem("name", name);
     localStorage.setItem("age", age);
     localStorage.setItem("accNo", accNo);
 
-    document.getElementById("detailsSaved").textContent =
-        "Details Saved Successfully";
-}
 
-
-function nextPage() {
-
+    // Open second page
     window.location.href = "moreDetails.html";
-
 }
 
 
+
+// Run this code only if we are on moreDetails.html
 
 if (document.getElementById("details")) {
 
@@ -27,17 +25,40 @@ if (document.getElementById("details")) {
     let age = localStorage.getItem("age");
     let accNo = localStorage.getItem("accNo");
 
-    document.getElementById("details").textContent =
-        `Name: ${name}, Age: ${age}, Account Number: ${accNo}`;
+
+    document.getElementById("details").innerHTML = `
+        Name: ${name}<br><br>
+        Age: ${age}<br><br>
+        Account Number: ${accNo}
+    `;
 }
 
 
-function submitDetails() {
 
-    let hireReason = document.getElementById("hireReason").value;
+function validateDetails() {
 
-    localStorage.setItem("hireReason", hireReason);
+    // Get values from localStorage
+    let name = localStorage.getItem("name");
+    let age = localStorage.getItem("age");
+    let accNo = localStorage.getItem("accNo");
 
-    document.getElementById("submitted").textContent =
-        "All Details Submitted Successfully";
+
+    // Put values permanently into the current webpage
+    document.getElementById("finalName").textContent = name;
+    document.getElementById("finalAge").textContent = age;
+    document.getElementById("finalAccNo").textContent = accNo;
+
+
+    // Hide validation section
+    document.getElementById("storedDetails").style.display = "none";
+
+
+    // Show final section
+    document.getElementById("validatedData").style.display = "block";
+
+
+    // Remove temporary localStorage data
+    localStorage.removeItem("name");
+    localStorage.removeItem("age");
+    localStorage.removeItem("accNo");
 }
